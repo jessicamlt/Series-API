e73535b2e535bd6493106237f12186aa4109e980
Jessica <jessica.mlt@hotmail.com>
2020-10-05 17:39:46 -0300

    SerieCell creation

---
+++ SeriesAPI/MainScreen/Cells/SerieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SerieCollectionViewCell.swift
+//  SeriesAPI
+//
+//  Created by Jéssica Trindade on 05/10/20.
+//
+
+import UIKit
+
+class SerieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
