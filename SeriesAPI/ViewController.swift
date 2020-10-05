d215dbd6291221307491eb7105ab009e74a9126a
Jessica <jessica.mlt@hotmail.com>
2020-10-05 17:20:37 -0300

    Initial project configuration

---
+++ SeriesAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeriesAPI
+//
+//  Created by Jéssica Trindade on 05/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
